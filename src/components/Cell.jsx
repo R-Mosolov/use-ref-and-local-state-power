d19b19a65a87a3f09b559a845a489f9a24c5dbaf
Roman Mosolov <R.V.Mosolov@ya.ru>
2023-12-15 15:42:10 +0300

    Add logic to re-render few (2 weeks) cells on mouse over and out

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
-import cn from 'classnames';
-
 export const Cell = ({ i, j, data }) => {
-  const [isChanged, setChange] = useState(false);
+  const handleMouseOver = (data) => {
+    for (let k = 0; k < 14; k++) {
+      const cell = document.querySelector(`.cell-${i}-${j+k}`);
+      cell?.classList.add('changed');
 
-  const handleChange = (data) => {
-    setChange(status => !status);
-    data[`${i}-${j}`] = `${i}-${j}`;
+      data[`${i}-${j+k}`] = `${i}-${j+k}`;
+    }
+
+    console.log('data', data);
+  };
+
+  const handleMouseOut = (data) => {
+    for (let k = 0; k < 14; k++) {
+      const cell = document.querySelector(`.cell-${i}-${j+k}`);
+      cell?.classList.remove('changed');
+
+      data[`${i}-${j+k}`] = `${i}-${j+k}`;
+    }
+
     console.log('data', data);
   };
 
   return (
     <div 
-      className={cn("cell", { 'clicked': isChanged })} 
-      onClick={() => handleChange(data)}
-      onMouseOver={() => handleChange(data)}
-      onMouseOut={() => handleChange(data)}
+      className={`cell cell-${i}-${j}`}
+      onMouseOver={() => handleMouseOver(data)}
+      onMouseOut={() => handleMouseOut(data)}
     >
       {`i=${i}, j=${j}`}
     </div>
